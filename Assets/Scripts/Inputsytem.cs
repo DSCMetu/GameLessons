bea5bb704ad5d4738c39b6fd12a0dbfb8b8156a2
TorchaSpace <ali.emre.celix@gmail.com>
2022-10-25 19:23:53 +0300

    Lesson 2

---
+++ Assets/Scripts/Inputsytem.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inputsytem : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = transform.right * horizontal + transform.forward * vertical;
+        transform.Translate(movement * speed * Time.deltaTime);
+
+        if(movement != Vector3.zero)
+        {
+            animator.SetBool("running", true);
+        }
+        else
+        {
+            animator.SetBool("running", false);
+        }
+    }
+}
