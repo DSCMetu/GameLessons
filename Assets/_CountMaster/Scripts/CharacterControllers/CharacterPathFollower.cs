a490e549ec8447a6d59ae39410025fbb72b1d8e6
gamedevdeniz <mdeniz.sener@gmail.com>
2022-11-08 18:33:54 +0300

    untill speed

---
+++ Assets/_CountMaster/Scripts/CharacterControllers/CharacterPathFollower.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterPathFollower : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+
+    private void Start()
+    {
+        
+    }
+}
